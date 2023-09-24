81ffb9d6d3ad2f6cf0cb203ab0b31d1b1c622ed6
Dario Lencina <dario@securityunion.dev>
2023-09-24 08:57:59 -0400

    adding redis

--- main.tf
+++ main.tf
@@ -172,3 +172,48 @@ resource "aws_db_instance" "postgres" {
     Name = "Neferdata"
   }
 }
+
+# Elasticache - Redis
+
+resource "aws_security_group" "elasticache" {
+  name        = "elasticache"
+  description = "Allow inbound traffic for ElastiCache Memcached"
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "redis_subnet_group"
+  subnet_ids = module.vpc.private_subnets
+  description = "ElastiCache subnet group"
+  tags = {
+    Name = "Neferdata"
+  }
+}
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id      = "neferdata-redis"
+  engine          = "redis"
+  node_type       = "cache.t2.micro"
+  num_cache_nodes = 1
+  port            = 6379
+
+  tags = {
+    Name = "Neferdata"
+  }
+
+  subnet_group_name = aws_elasticache_subnet_group.redis_subnet_group.name
+  security_group_ids = [aws_security_group.elasticache.id]
+}
